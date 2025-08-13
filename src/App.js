c82ec107cee7caa3ee2bd7b534e942a46df5ed26
Amelia Dutta <amelia280111dutta@gmail.com>
2025-08-13 20:02:23 +0530

    added loading functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState,useEffect } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -7,15 +8,38 @@ import Footer from './components/Footer';
 import './App.css';
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setIsLoaded(true);
+    };
+
+    if (document.readyState === 'complete') {
+      handleLoad();
+    } else {
+      window.addEventListener('load', handleLoad);
+      return () => window.removeEventListener('load', handleLoad);
+    }
+  }, []);
+
   return (
-    <div className='bg-[#FFFAFA] z-0'>
-      <Header />
-      <Hero />
-      <About />
-      <Portfolio />
-      <Contact />
-      <Footer />
-    </div>
+    isLoaded ? (
+        <div className='bg-[#FFFAFA] z-0'>
+          <Header />
+          <Hero />
+          <About />
+          <Portfolio />
+          <Contact />
+          <Footer />
+        </div>
+      )
+      :
+      (
+      <div className="flex justify-center items-center min-h-screen">
+        <img src={require("./assets/loader.gif")} alt="loader" />
+      </div>
+    )
   );
 }
 
