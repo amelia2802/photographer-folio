7b9db5d9fac0fe35371f0d31b634f378675c3701
Amelia Dutta <amelia280111dutta@gmail.com>
2025-08-15 19:36:08 +0530

    Fix bugs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ export default function Footer(){
 
     return(
         <footer className="md:flex flex-col h-[100%] bg-[#ead5c4] mt-14 p-6">
-            <div className="flex max-[600px]:flex-col items-center justify-between px-12">
-                <div className='flex items-center justify-center mt-5 max-[600px]:mb-5'>
+            <div className="flex max-[600px]:flex-col items-center justify-between">
+                <a className='flex items-center justify-center mt-5 max-[600px]:mb-5' href="/">
                     <h1 className={`font-custom relative text-lg -right-5 md:-right-14 text-[#D40003] font-semibold md:text-xl`}>BARBARA WILLIAMS</h1>
                     <img className='w-[1.5em] md:w-[2.5em] md:left-6 h-auto object-cover -top-4 md:-top-6 relative ' src={  require("../assets/alt-icon.png")} alt="logo" />
-                </div>
+                </a>
                 <div className="flex gap-5">
                     <a className="flex text-2xl p-2 rounded-full bg-[#f03f4295] text-[#D40003] hover:submit" href={contactInfo.email}>
                         <PiEnvelopeThin />
@@ -24,7 +24,7 @@ export default function Footer(){
                         <PiInstagramLogoThin />
                     </a>
                 </div>
-                <nav>
+                <nav className="pr-6">
                     <ul className="flex gap-5 mt-5">
                         <li className="hover:nav-link"><a href="#about">About</a></li>
                         <li className='hover:nav-link'><a href="#portfolio">Portfolio</a></li>
--- src/components/Header.js
+++ src/components/Header.js
@@ -21,12 +21,12 @@ export default function Header(){
     }
     return(
         <header className={`z-10 w-full h-[7em]  m-0 flex md:items-center justify-between fixed ${isTop ? "" : "shadow-lg"} ${isTop ? "" : "bg-[#fffafaf7]"}`}>
-            <div className='flex items-center justify-center'>
+            <a className='flex items-center justify-center' href="/">
                 <h1 className={`font-custom relative text-sm -right-5 md:-right-14 ${isTop ? "text-[#F9ECE3]" : "text-[#D40003]"} font-semibold md:text-xl`}>BARBARA WILLIAMS</h1>
                 <img className='w-[1.5em] md:w-[2.5em] md:left-6 h-auto object-cover -top-4 md:-top-6 relative ' src={isTop ? require("../assets/icon.png") : require("../assets/alt-icon.png")} alt="logo" />
-            </div>
+            </a>
             <nav className="flex flex-col gap-5 pr-12">
-                <button onClick={toggleMenu} className="relative z-10 top-12 left-5 p-2 text-[#FF4649] rounded-full text-xl hover:text-[#D40003] md:hidden"><RxHamburgerMenu /></button>
+                <button onClick={toggleMenu} className="relative z-10 top-9 left-5 p-2 text-[#FF4649] rounded-full text-xl hover:text-[#D40003] md:hidden"><RxHamburgerMenu /></button>
                 <ul className={`max-[600px]:absolute max-[600px]:right-0 max-[600px]:py-24 max-[600px]:shadow-lg flex gap-5 ${isTop ? "text-neutral-50" : ""} ${open ? "max-[600px]:flex" : "max-[600px]:hidden"} max-[600px]:flex-col max-[600px]:items-center max-[600px]:bg-[#fffafaf7] max-[600px]:text-gray-900 max-[600px]:p-5 `}>
                     <li className="hover:nav-link"><a href="#about">About</a></li>
                     <li className='hover:nav-link'><a href="#portfolio">Portfolio</a></li>
